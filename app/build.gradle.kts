c544f779ee35413d40ca1bf0fba6397f66a1110d
Michael Whapples <software@mdw.onl>
2026-04-03 13:53:24 +0100

    Update some dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "app.audionav.compass"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
